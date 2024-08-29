9aba7de4176bc1a39e3409f2eb66a02b8d9fd53f
victoralbucorde <victoralbucorde@gmail.com>
2024-08-29 10:57:10 -0300

    day of the week complete date and complete time added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
-const diaSemana = document.getElementById(dia-semana);
-const diaMesAno = document.getElementById(dia-mes-ano);
-const horaMinSec = document.getElementById(hora-min-sec);
+const diaSemana = document.getElementById("dia-semana");
+const diaMesAno = document.getElementById("dia-mes-ano");
+const horaMinSec = document.getElementById("hora-min-sec");
+const arrayDayWeek = new Array("Domingo", "Segunda-Feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado")
+
+
+function dayWeek(){
+    const date = new Date();
+    return arrayDayWeek[date.getDay()]
+}
+
+function completeDate(){
+    const date = new Date();
+    return addZero(date.getDate()) + "/" + addZero(date.getMonth() + 1) + "/" + date.getFullYear()
+}
+
+function completeTime(){
+    const date = new Date();
+    return addZero(date.getHours()) + ":" + addZero(date.getMinutes()) + ":" + addZero(date.getSeconds())
+}
+
+function addZero(i){
+    if (i<10) {i = "0" + i}
+    return i
+}
+
+diaMesAno.textContent = completeDate();
+horaMinSec.textContent = completeTime();
+diaSemana.textContent = dayWeek();
