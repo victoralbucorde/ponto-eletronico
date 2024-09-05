e24aee30939ea19bb7cce64f6c965c1ad3caeccb
victoralbucorde <victoralbucorde@gmail.com>
2024-09-05 10:58:00 -0300

    dialog created

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
-const diaSemana = document.getElementById("dia-semana");
-const diaMesAno = document.getElementById("dia-mes-ano");
-const horaMinSec = document.getElementById("hora-min-sec");
+//First page
+const diaSemana = document.getElementById("day-week");
+const diaMesAno = document.getElementById("day-month-year");
+const horaMinSec = document.getElementById("hour-min-sec");
+const clockInButton = document.getElementById("clock-in");
+const clockInHistoryButton = document.getElementById("clock-in-history");
 const arrayDayWeek = new Array("Domingo", "Segunda-Feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado")
+diaMesAno.textContent = completeDate();
+diaSemana.textContent = dayWeek();
+setInterval(updateTime,1000);
+clockInButton.addEventListener("click",()=>{dialogClockIn.showModal();});
 
+//Dialog
+const dialogClockIn = document.getElementById("dialog-clock-in");
+const dialogCloseButton = document.getElementById("close");
+const startButton = document.getElementById("start");
+const dialogDate = document.getElementById("dialog-date");
+const dialogTime = document.getElementById("dialog-time");
+startButton.addEventListener("click",()=>{});
+dialogTime.textContent = "Data: " + completeDate();
+setInterval(()=>{dialogDate.textContent="Hora: " + completeTime();},1000);
+dialogCloseButton.addEventListener("click",()=>{dialogClockIn.close();});
+
+//Functions
+function updateTime(){
+    horaMinSec.textContent = completeTime();
+}
 
 function dayWeek(){
     const date = new Date();
@@ -23,7 +45,3 @@ function addZero(i){
     if (i<10) {i = "0" + i}
     return i
 }
-
-diaMesAno.textContent = completeDate();
-horaMinSec.textContent = completeTime();
-diaSemana.textContent = dayWeek();
